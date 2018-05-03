a1294bee6ad21f316e39bf872c097f4a6834d559
hsiaojung <jung1997c@gmail.com>
2018-05-03 21:29:10 +0800

    upload app code & picture

---
+++ shoppingApp/shoppingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shoppingApp
+//
+//  Created by User12 on 2018/5/3.
+//  Copyright © 2018年 User12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
